77f97e3c95a52d78cc1e2d8b4ab67c3addf52f7e
Joel Cheng <joelcheng.work@gmail.com>
2012-12-20 17:11:46 +0800

    Animated part module hopefully working.

--- Source/KSPTest/KSPTest/TestModule.cs
+++ Source/KSPTest/KSPTest/TestModule.cs
@@ -13,36 +13,36 @@ namespace KSPTest
         [KSPField]
         public string key;
 
-        Animation[] clips;
-
-        int count = 0;
+        Animation anim;
 
         public override void OnStart(PartModule.StartState state)
         {
-            //Gets all the animation clips 
-            clips = part.FindModelAnimators(clipName);
-            foreach (Animation anim in clips)
-            {
-                anim[clipName].wrapMode = WrapMode.ClampForever;
-                anim.Stop();
-            }
+            anim = part.FindModelAnimators(clipName)[0];
+            //anim[clipName].wrapMode = WrapMode.ClampForever;
+            //Ensure speed is positive on first playing
+            anim[clipName].speed = -1;
+            anim.Stop();
         }
-        //God damnit I had this working before trying to read config values
         public override void OnUpdate()
         {
             KeyCode inputKey;
-            inputKey = (KeyCode)Enum.Parse(typeof(KeyCode),key);
-                       
+            //Parse config value into KeyCode
+            inputKey = (KeyCode)Enum.Parse(typeof(KeyCode), key);
+
+            //<rant>Clampmode isn't working for some reason.</rant>
+            //If overrun length of animation, reset to start/end
+            float time = anim[clipName].normalizedTime;
+            if (time >= 1 || time <= 0)
+                anim[clipName].normalizedTime = (int)time;
+
+            PDebug.Log(anim[clipName].normalizedTime);
+            
+            //Play animation. Each time invert direction
             if (Input.GetKeyDown(inputKey))
             {
-                PDebug.Log(count + "Keydown");
-                ++count;
-                foreach (Animation anim in clips)
-                {
                     AnimationState state = anim[clipName];
                     state.speed *= -1;
                     anim.Play(clipName);
-                }
             }
             base.OnUpdate();
         }
